c8e446835cfe2e206d19aff49c92f67dcdec5404
CodingCCarpenter <christineassists@aol.com>
2019-10-08 20:43:59 -0500

    MovieCard.js for MVP

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
-const MovieCard = props => {
-  return;
-};
+function MovieCard ({title, director, metascore, stars}) {
+  return(
+    <div className="movie-card">
+      <h2>{title}</h2>
+    <div className="movie-director">
+      <p>Director: <em>{director}</em></p>
+    </div>
+    <div className="movie-metascore">
+      <p>Metascore: <strong>{metascore}</strong></p>
+    </div>
+    <h3>Actors</h3>
+
+    {stars.map((star) => (
+      <div key={star} className="movie-star">
+        {star}
+      </div>
+    ))}
+  </div>
+);
+}
+    
+    
 
 export default MovieCard;
